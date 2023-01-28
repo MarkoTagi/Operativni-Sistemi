ab43dd59da3d9690a8aebfee8eb26e5894dabf62
MarkoTagi <markotagi@gmail.com>
2023-01-28 20:51:42 +0100

    Prethodna implementacija nije radila. Ispravljeno.

--- 2022_Oktobar2/Zadatak4/pronadjiDatoteku.c
+++ 2022_Oktobar2/Zadatak4/pronadjiDatoteku.c
@@ -7,7 +7,7 @@
 #include <errno.h>
 #include <sys/stat.h>
 
-bool findFile(const char* directorypPath, const char* fileName);
+void findFile(const char* directorypPath, const char* fileName);
 
 int main(int argc, char** argv) {
     if (argc != 3) {
@@ -23,19 +23,17 @@ int main(int argc, char** argv) {
         perror(NULL);
         exit(EXIT_FAILURE);
     }
-    bool wasFound  = findFile(directoryPath, fileName);
-    if (!wasFound) printf("The file [%s] does not exist inside of the directory [%s]\n", fileName, directoryPath);
+    findFile(directoryPath, fileName);
     closedir(directoryPointer);
 }
 
-bool findFile(const char* directoryPath, const char* fileName) {
+void findFile(const char* directoryPath, const char* fileName) {
     DIR* directoryPointer = opendir(directoryPath);
     if (directoryPointer == NULL) {
         fprintf(stderr, "Failed to open directory [%s]\n\tReason: ", directoryPath);
         perror(NULL);
         exit(EXIT_FAILURE);
     }
-    bool wasFound = false;
     struct dirent* directoryEntry = (struct dirent*)malloc(sizeof(struct dirent));
     struct stat* fileStats = (struct stat*)malloc(sizeof(struct stat));
     while ((directoryEntry = readdir(directoryPointer)) != NULL) {
@@ -48,16 +46,14 @@ bool findFile(const char* directoryPath, const char* fileName) {
             perror(NULL);
             exit(EXIT_FAILURE);
         }
-        if (S_ISDIR(fileStats->st_mode)) return findFile(newPath, fileName); // Nalazi prvu pojavu i izlazi, ukloni "return" ako zelis da pronadje sve fajlove, ali nece da vrati informaciju da li je nasao fajl.
+        if (S_ISDIR(fileStats->st_mode)) findFile(newPath, fileName);
         if (S_ISREG(fileStats->st_mode) && strcmp(fileName, directoryEntry->d_name) == 0) {
             printf("Found file [%s]!\n", fileName);
             printf("  Location: %s\n", newPath);
             printf("  Size: %ld\n", fileStats->st_size);
-            return true;
         }
     }
     closedir(directoryPointer);
     free(directoryEntry);
     free(fileStats);
-    return wasFound;
 }
